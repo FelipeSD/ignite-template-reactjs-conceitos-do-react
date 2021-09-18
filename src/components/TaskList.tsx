2080f299c614fa4955617d686ad829544f131180
Felipe Damasceno <felipe@superempresa.com.br>
2021-09-18 17:24:33 -0300

    challenge completed

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,40 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(newTaskTitle.trim() === ''){ 
+      alert("Não é possível criar uma tarefa");
+      return
+    };
+
+    const newTask: Task = {
+      id: Math.floor(Math.random() * 10000),
+      title: newTaskTitle,
+      isComplete: false,
+    }
+
+    setTasks(prevTasks => [...prevTasks, newTask]);
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const newTaskList = [...tasks];
+    newTaskList.map(task => {
+      if(task.id === id){
+        task.isComplete = !task.isComplete;
+      }
+    });
+    setTasks(newTaskList);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const newTaskList = [...tasks];
+    newTaskList.map(task => {
+      if(task.id === id){
+        newTaskList.splice(tasks.indexOf(task), 1);
+      }
+    })
+    setTasks(newTaskList);
   }
 
   return (
